e9828f0cf688cd77fb577608249300d1562a9c1a
mingxm <mingxm@esensoft.com>
2011-03-22 18:42:56 +0800

    提交今天下午写的一些代码，主要是实现报表设计器原型中的属性编辑器部分

--- src/main/webapp/js/rptdsn/editorcontrol.js
+++ src/main/webapp/js/rptdsn/editorcontrol.js
@@ -1,3 +1,48 @@
 /**
  * 
  */
+ 
+ function ReportDesign(){
+ 	this.type = "ana_report";
+ 	this.hideGrid = false;
+ 	this.name = "";
+ 	this.caption = "";
+ 	this.rptid = "";
+ 	this._init();
+ }
+
+ ReportDesign.prototype._init = function(){
+ 	this.pControl = new propertyEditorControl($(".eastproperties"),this);
+ }
+ 
+ ReportDesign.prototype.getName = function(){
+ 	return this.name;
+ }
+ 
+ ReportDesign.prototype.setName = function(value){
+ 	this.name = value;
+ }
+ 
+ ReportDesign.prototype.getCaption = function(){
+ 	return this.caption;
+ }
+ 
+ ReportDesign.prototype.setCaption = function(value){
+ 	this.caption = value;
+ }
+ 
+ ReportDesign.prototype.isHiddenGrid = function(){
+ 	return !!this.hideGrid;
+ }
+ 
+ ReportDesign.prototype.hideGrid = function(value){
+ 	this.hideGrid = !!value;
+ }
+ 
+ ReportDesign.prototype.getType = function(){
+ 	return this.type;
+ }
+ 
+ ReportDesign.prototype.setType = function(value){
+ 	this.type = value;
+ }
--- src/main/webapp/js/rptdsn/property-editor-control.js
+++ src/main/webapp/js/rptdsn/property-editor-control.js
@@ -1,17 +1,117 @@
 /**
  * 
  */
- function propertyEditorControl(container){
+ function propertyEditorControl(container,reportControl){
  	this.container = container;
- 	this.obj = false;
- 	this.type = false;
+ 	this.reportControl = reportControl;
+ 	this.obj = reportControl;
+ 	this.type = "report";
+ 	this._init();
  }
  
  propertyEditorControl.prototype._init = function(){
- 	this.switchByObj()
+ 	this._updateDom()
  }
  
  propertyEditorControl.prototype.switchByObj = function(obj,type){
  	this.obj = obj;
  	this.type = type;
+ 	this._updateDom();
  }
+ 
+ propertyEditorControl.prototype._updateDom = function(){
+ 	this.container.empty();
+ 	switch(this.type){
+ 		case "text":
+ 		case "table":
+ 		case "flashchart":
+ 		case "ditu":
+ 		case "report":
+ 			this.updateDom_report();
+ 			return;
+ 		case "":
+ 			this.updateDom_report();
+ 			return;
+ 	}
+ }
+ 
+ propertyEditorControl.prototype.updateDom_report = function() {
+ 	var _self = this;
+	this.container.jproeditor({
+		    defaultType	: 'type',
+		    items		    : [{
+			        title		: '报表名称',
+			        type		: 'edit',
+			        defaultValue:_self.reportControl.getName(),
+			        events	: [{
+				            name	: 'change',
+				            fn		: function(e) {
+					            var obj = e.data;
+					            _self.reportControl.setName(obj.attr("value"));
+				            }
+			            }]
+		        }, {
+			        title		: '报表标题',
+			        type		: 'edit',
+			        defaultValue:_self.reportControl.getCaption(),
+			        events	: [{
+				            name	: 'change',
+				            fn		: function(e) {
+					            var obj = e.data;
+					            _self.reportControl.setCaption(obj.attr("value"));
+				            }
+			            }]
+		        }, {
+			        title		: '隐藏表格',
+			        type		: 'checkbox',
+			        defaultValue:_self.reportControl.isHiddenGrid(),
+			        events	: [{
+				            name	: 'change',
+				            fn		: function(e) {
+					            var obj = e.data;
+					            _self.reportControl.hiddenGrid(obj.attr("checked"));
+				            }
+			            }]
+		        }, {
+			        title		: '报表类型',
+			        type		: 'radio',
+			        name		: 'report_type',
+			        defaultValue:_self.reportControl.getType(),
+			        items		: [{
+				            value		: 'ana_document',
+				            caption	: '分析报告',
+				            checked	: true
+			            }, {
+				            value		: 'ana_report',
+				            caption	: '分析表'
+			            }, {
+				            caption	: '主题表',
+				            value		: 'dwsubject'
+			            }],
+			        events	: [{
+				            name	: 'click',
+				            fn		: function(e) {
+					            _self.reportControl.setType(e.data.attr("value"));
+				            }
+			            }]
+		        }]
+	    });
+}
+ 
+ propertyEditorControl.prototype.updateDom_table = function(){
+ 	
+ }
+ 
+ propertyEditorControl.prototype.updateDom_edit = function(){
+ 	
+ }
+ 
+ propertyEditorControl.prototype.updateDom_flash = function(){
+ 	
+ }
+ 
+ propertyEditorControl.prototype.updateDom_ditu = function(){
+ 	
+ }
+ 
+ 
