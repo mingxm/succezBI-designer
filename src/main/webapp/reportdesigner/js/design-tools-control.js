ac2b7c1dfc4bbcd4ef88b8b3a22ee03bb39d8f57
wangyg <498812492@qq.com>
2011-04-11 18:10:03 +0800

    完善控件列表，实现拖动控件添加

--- src/main/webapp/reportdesigner/js/design-tools-control.js
+++ src/main/webapp/reportdesigner/js/design-tools-control.js
@@ -8,10 +8,25 @@
 	$.fn.DesignToolsControl = function(owner, setting){
 		_owner = owner;
 		
-		var _container = this;
-		var _controls = $("li", _container);
+		var _container = this,
+		    _controls = $("li", _container),
+		    _toolContainer =$("<div id = 'toolContainer'><ul><li>tool container</li></ul></div>"),
+			controlMoved = false,
+			divLayOutCenter = $(".ui-layout-center",document.body),
+			divLayOutWest = $(".ui-layout-west",document.body),
+			offsetX = 10,
+			offsetY = 10;
+
+			
 		
 		$.extend(_container, {
+			_init:function(){
+				//设置工具容器位于最上方，从而可以显示出来
+				_toolContainer.css({"position":"absolute","z-index":"999","width":"","height":"","left":"100px","top":"100px","filter":"alpha(opacity=90)","opacity":"0.9"}); 
+				//初始化的时候同时初始化所需的容器
+				_toolContainer.appendTo(document.body);
+				_toolContainer.hide();			
+			},
 			/*
 			 * 初始化绑定事件
 			 */
@@ -26,24 +41,64 @@
 						if (self.hasClass("active")) return;
 						self.css("background-color","#fff");
 					}).click(function(){
-						_container._setSelected(this);												
-						return false;
+						return false;												
 					})				
 					/*
 				 	 * 绑定鼠标的按下事件，在该事件中还要绑定鼠标的移动事件
 				 	 */
-					.mousedown(function(){
-						var cloneLi = self.clone();
+					.mousedown(function(event){
+						var mousedownPoint = {x:event.pageX,y:event.pageY};												
+						_container._setSelected(this);	
+						var cloneLi = self.clone();	
+						cloneLi.css({
+							"background-color": "#ccc",
+							"color": "#ccc",
+							"width":"150px",
+							"background-position":mousedownPoint.x,
+							"border":"2px solid #cacaff"
+						});											
+						$("#toolContainer").find("li").replaceWith(cloneLi);
+						$(document).bind("mousemove.toolContainer",function(event){
+							//光标移动一定的距离的情况下才认为要添加控件
+							if ((Math.abs(mousedownPoint.x - event.pageX) >20) || (Math.abs(mousedownPoint.y - event.pageY)>20))
+							{
+								_toolContainer.show("fast");
+								controlMoved =true;
+							}
+							_toolContainer.css("left",event.pageX-mousedownPoint.x);
+							_toolContainer.css("top",event.pageY);
+						}).bind("mouseup.toolContainer",function(event){
+							_toolContainer.hide("fast");
+							if (controlMoved) {
+								var topLeft = divLayOutCenter.offset(),
+									right = topLeft.left + divLayOutCenter.width(),
+									bottom = topLeft.top + divLayOutCenter.height();
+								if ((event.pageX > topLeft.left) && (event.pageX < right) && (event.pageY > topLeft.top) && (event.pageY < bottom)) {
+									_owner.addComponent(self.attr("objName"),{
+										x:event.pageX-divLayOutWest.width()+15,
+										y:event.pageY-8
+									});
+								}
+								controlMoved = false;
+							}
+						});							
 					})
 					.dblclick(function(){
+						_owner.addComponent(self.attr("objName"),{
+							x : offsetX,
+							y : offsetY							
+						})
+						offsetX +=10;
+						offsetY +=10;
 						return false;
-					})
-					.mouseup(function(){
-						
 					})
 					.bind("selectstart",function(){
 						return false;
 					});					
+				});
+				//必须要在鼠标弹起的时候解除mousedown的时候进行的事件绑定
+				$(document).mouseup(function(){
+					$(document).unbind(".toolContainer");
 				});
 			},
 			/*
@@ -81,7 +136,7 @@
 					_container._clearSelected();
 				}																						
 				$(control).css({
-							"background-color":"#8080ff",
+							"background-color":"#8080FF",
 							"border":"1px,solid,#000"
 						});
 				$(control).addClass("active");				
@@ -97,6 +152,7 @@
 			
 		});
 		
+		_container._init();
 		_container._initEvent();
 		return _container;		
 	}	
