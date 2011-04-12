f759c0b94dcf7ac394bae811ef9054691980ddcb
mingxm <mingxm@esensoft.com>
2011-04-12 11:02:53 +0800

    解决表格内框选表元和合并拆分的问题，存在多个表格时框选还有问题

--- src/main/webapp/reportdesigner/js/table-editor-control.js
+++ src/main/webapp/reportdesigner/js/table-editor-control.js
@@ -49,8 +49,7 @@
 	var _self = this;
 	this.container.contextMenu("tableMenu",{
 			      	onContextMenu:function(e){
-			      		_self.menuCell = $(e.target);
-			      		return _self.menuCell.is("td");
+			      		return $(e.target).is("td");
 			      	},
 			      	bindings:{
 			      		"split":function(t){
@@ -116,9 +115,11 @@
  TableEditorControl.prototype.doClick = function(e){
  	log("开始执行TableEditorControl.prototype.doClick方法");
 	var t1 = new Date().getTime();
+	this.selectCell = false;
  	var item = $(e.target);
  	if(item.is("td")){
  		this.owner.endEdit();
+ 		this.selectCell = item;
  		new CellEditor(item,this).onClick();
  	}else if(item.is("th") || item.is("div")) {
  		this.owner.select(this);
@@ -137,9 +138,19 @@
  }
  
  TableEditorControl.prototype.selectCellByRect = function(r){
- 	this.selection = this.getSelectionByRect(r);
- 	var rect = this.getRectBySelection(this.selection);
- 	this.owner.selectSelectionCells(rect,this);
+ 	this.owner.endEdit();
+ 	var s = this.getSelectionByRect(r);
+	if (this.isACellSelection(s)) {
+		this.selectCell = this.getCell(s.top, s.left);
+		var obj = new CellEditor(this.selectCell, this);
+		this.owner.select(obj);
+	}
+	else {
+		this.selectCell = false;
+		this.selection = this.getSelectionByRect(r);
+		var rect = this.getRectBySelection(this.selection);
+		this.owner.selectSelectionCells(rect, this);
+	}
  }
  
  TableEditorControl.prototype.getSelectionByRect = function(r){
@@ -152,8 +163,8 @@
  }
  
  TableEditorControl.prototype.getRectBySelection = function(s){
- 	var c1 = this.cells[s.top][s.left];
- 	var c2 = this.cells[s.bottom][s.right];
+ 	var c1 = this.getCell(s.top,s.left);
+ 	var c2 = this.getCell(s.bottom,s.right);
  	var l = this.getLeft();
  	var t = this.getTop();
  	var p1 = c1.position();
@@ -171,34 +182,50 @@
  	var er = this.selection.bottom;
  	var fc = this.selection.left;
  	var ec = this.selection.right;
+ 	var mainCell = this.cells[fr][fc];
  	for(var i=fr;i<=er;i++){
  		var row = this.cells[i];
  		for(var j=fc;j<=ec;j++){
+ 			var cell = row[j];
+ 			if(cell.attr("rowSpan")>1 || cell.attr("colSpan")>1){
+ 				this.splitCell(cell);
+ 			}
  			if(j == fc && i == fr){
- 				row[j].attr("rowSpan",er-fr+1).attr("colSpan",ec-fc+1);
+ 				cell.attr("rowSpan",er-fr+1).attr("colSpan",ec-fc+1);
  			}else {
- 				row[j].hide();
+ 				cell.data("maincell",mainCell);
+ 				cell.hide();
  			}
  		}
  	}
+ 	this.selection = false;
+ 	this.selectCell = mainCell;
  }
  
- TableEditorControl.prototype.splitCell = function(){
- 	log("开始执行TableEditorControl.prototype.splitCell方法");
-	var t1 = new Date().getTime();
- 	if(!this.menuCell) return;
- 	if(!this.menuCell.is(":visible")) return;
- 	var colSpan = this.menuCell.attr("colSpan");
- 	var rowSpan = this.menuCell.attr("rowSpan");
- 	var col = this.menuCell.attr("cellIndex");
- 	var row = this.menuCell.parent().attr("rowIndex")-1;
- 	this.menuCell.attr("colSpan",1).attr("rowSpan",1);
+ TableEditorControl.prototype.splitCell = function(cell){
+ 	if(!cell){
+ 		cell = this.selectCell;
+ 	}
+ 	if(!cell) return;
+ 	var colSpan = cell.attr("colSpan");
+ 	var rowSpan = cell.attr("rowSpan");
+ 	var col = cell.attr("cellIndex");
+ 	var row = cell.parent().attr("rowIndex")-1;
+ 	cell.attr("colSpan",1).attr("rowSpan",1);
  	for(var i=row;i<row+rowSpan;i++){
  		for(var j=col;j<col+colSpan;j++){
- 			this.cells[i][j].show();
+ 			var c = this.cells[i][j];
+ 			c.removeData("maincell");
+ 			c.show();
  		}
  	}
- 	log("执行TableEditorControl.prototype.splitCell完毕！耗时"+(new Date().getTime()-t1)+"ms");
+ 	this.selectCell = false;
+ 	this.selection = {
+ 		left:col,
+ 		right:col+colSpan-1,
+ 		top:row,
+ 		bottom:row+rowSpan-1
+ 	}
  }
  
  TableEditorControl.prototype.addRow = function(){
@@ -219,6 +246,11 @@
  
  TableEditorControl.prototype.getRowHeight = function(index){
  	if(this.cells.length <= index) return 0;
+ 	var rs = this.cells[index];
+ 	for(var i=0;i<rs.length;i++){
+ 		var cell = rs[i];
+ 		if(cell.is(":visible") && cell.attr("rowSpan") == 1) return cell.height();
+ 	}
  	return this.cells[index][0].parent().height();
  }
  
@@ -236,33 +268,57 @@
  	return this.container.find("th:eq("+index+")").width();
  }
  
+ /**
+  * 获取某一坐标所位于的行号
+  * @param {} p
+  * @return {Number}
+  */
  TableEditorControl.prototype.getRowIndexByPos = function(p){
  	var r = this.getRect();
  	var top = r.top+20;
  	var bottom = r.top + r.height;
  	if(p<=top) return 0;
- 	if(p>=bottom) return this.getRowCount();
+ 	if(p>=bottom) return this.getRowCount()-1;
  	var len = this.getRowCount();
  	var count = top;
  	for(var i=0;i<len;i++){
  		count += this.getRowHeight(i);
  		if(count>=p) return i;
  	}
- 	return len;
+ 	return len-1;
  }
  
+ /**
+  * 获取某一坐标所位于的列号
+  * @param {} p
+  * @return {Number}
+  */
  TableEditorControl.prototype.getColIndexByPos = function(p){
  	var r = this.getRect();
  	var left = r.left;
  	var right = left + r.width;
  	if(p<=left) return 0;
- 	if(p>=right) return this.getColCount();
+ 	if(p>=right) return this.getColCount()-1;
  	var len = this.getColCount();
  	var count = left;
  	for(var i=0;i<len;i++){
  		count += this.getColWidth(i);
  		if(count>=p) return i;
  	}
- 	return len;
+ 	return len-1;
+ }
+ 
+ TableEditorControl.prototype.getCell = function(row,col){
+ 	var cell = this.cells[row][col];
+ 	return cell.is(":visible")?cell:cell.data("maincell");
+ }
+ 
+ /**
+  * 判断一个区域刚好是一个表元区域
+  * @param {} s
+  * @return {}
+  */
+ TableEditorControl.prototype.isACellSelection = function(s){
+ 	return this.getCell(s.top,s.left) == this.getCell(s.bottom,s.right);
  }
  
